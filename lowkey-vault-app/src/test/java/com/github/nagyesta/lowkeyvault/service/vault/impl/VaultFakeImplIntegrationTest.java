ff2845b14282c4cb5e599a4d60524db3137b9093
Esta Nagy <nagyesta@gmail.com>
2024-05-10 16:42:15 +0200

    Fixes flaky tests
    {patch}
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- lowkey-vault-app/src/test/java/com/github/nagyesta/lowkeyvault/service/vault/impl/VaultFakeImplIntegrationTest.java
+++ lowkey-vault-app/src/test/java/com/github/nagyesta/lowkeyvault/service/vault/impl/VaultFakeImplIntegrationTest.java
@@ -12,13 +12,14 @@ import com.github.nagyesta.lowkeyvault.service.secret.impl.KeyVaultSecretEntity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+import java.time.Month;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.Deque;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static com.github.nagyesta.lowkeyvault.TestConstantsCertificates.CERT_NAME_1;
 import static com.github.nagyesta.lowkeyvault.TestConstantsUri.HTTPS_LOCALHOST;
@@ -30,9 +31,11 @@ import static java.time.temporal.ChronoUnit.MONTHS;
 
 class VaultFakeImplIntegrationTest {
 
-    private static final int INT_800 = 800;
+    private static final int INITIAL_OFFSET_IN_DAYS = 800;
+    private static final int EXTRA_OFFSET_IN_DAYS = 15;
+    private static final int INT_MORE_THAN_TWO_YEARS_IN_DAYS = getMoreThanTwoYearsAvoidingLeapYear();
     private static final int SECONDS_IN_1_DAY = 24 * 3600;
-    private static final int SECONDS_IN_800_DAYS = INT_800 * SECONDS_IN_1_DAY;
+    private static final int SECONDS_IN_MORE_THAN_TWO_YEARS = INT_MORE_THAN_TWO_YEARS_IN_DAYS * SECONDS_IN_1_DAY;
     public static final int EXPECTED_VERSIONS_AFTER_RENEWAL = 3;
 
     @Test
@@ -58,19 +61,19 @@ class VaultFakeImplIntegrationTest {
         ));
 
         //when
-        underTest.timeShift(SECONDS_IN_800_DAYS, true);
+        underTest.timeShift(SECONDS_IN_MORE_THAN_TWO_YEARS, true);
 
         //then
         final Deque<String> versions = underTest.certificateVaultFake().getEntities().getVersions(originalCertId);
         final List<ReadOnlyKeyVaultCertificateEntity> entities = versions.stream()
                 .map(v -> new VersionedCertificateEntityId(originalCertId.vault(), originalCertId.id(), v))
                 .map(certificateVaultFake.getEntities()::getReadOnlyEntity)
-                .collect(Collectors.toList());
+                .toList();
         Assertions.assertEquals(EXPECTED_VERSIONS_AFTER_RENEWAL, entities.size());
         final ReadOnlyKeyVaultCertificateEntity recreatedOriginal = entities.get(0);
         final ReadOnlyKeyVaultCertificateEntity firstRenewal = entities.get(1);
         final ReadOnlyKeyVaultCertificateEntity secondRenewal = entities.get(2);
-        assertTimestampsAreAdjustedAsExpected(approxNow, recreatedOriginal, INT_800);
+        assertTimestampsAreAdjustedAsExpected(approxNow, recreatedOriginal, INT_MORE_THAN_TWO_YEARS_IN_DAYS);
         final OffsetDateTime firstRenewalDay = recreatedOriginal.getExpiry().map(v -> v.minusDays(triggerThresholdDays)).orElseThrow();
         assertTimestampsAreAdjustedAsExpected(approxNow, firstRenewal, DAYS.between(firstRenewalDay, approxNow));
         final OffsetDateTime secondRenewalDay = firstRenewal.getExpiry().map(v -> v.minusDays(triggerThresholdDays)).orElseThrow();
@@ -106,14 +109,14 @@ class VaultFakeImplIntegrationTest {
         issuancePolicy.setContentType(CertContentType.PEM);
 
         //when
-        underTest.timeShift(SECONDS_IN_800_DAYS, true);
+        underTest.timeShift(SECONDS_IN_MORE_THAN_TWO_YEARS, true);
 
         //then
         final Deque<String> versions = underTest.certificateVaultFake().getEntities().getVersions(originalCertId);
         final List<ReadOnlyKeyVaultCertificateEntity> entities = versions.stream()
                 .map(v -> new VersionedCertificateEntityId(originalCertId.vault(), originalCertId.id(), v))
                 .map(certificateVaultFake.getEntities()::getReadOnlyEntity)
-                .collect(Collectors.toList());
+                .toList();
         Assertions.assertEquals(EXPECTED_VERSIONS_AFTER_RENEWAL, entities.size());
         assertRenewalUsedPem(underTest, entities.get(1));
         assertRenewalUsedPem(underTest, entities.get(2));
@@ -143,19 +146,19 @@ class VaultFakeImplIntegrationTest {
         ));
 
         //when
-        underTest.timeShift(SECONDS_IN_800_DAYS, true);
+        underTest.timeShift(SECONDS_IN_MORE_THAN_TWO_YEARS, true);
 
         //then
         final Deque<String> versions = underTest.certificateVaultFake().getEntities().getVersions(originalCertId);
         final List<ReadOnlyKeyVaultCertificateEntity> entities = versions.stream()
                 .map(v -> new VersionedCertificateEntityId(originalCertId.vault(), originalCertId.id(), v))
                 .map(certificateVaultFake.getEntities()::getReadOnlyEntity)
-                .collect(Collectors.toList());
+                .toList();
         Assertions.assertEquals(EXPECTED_VERSIONS_AFTER_RENEWAL, entities.size());
         final ReadOnlyKeyVaultCertificateEntity recreatedOriginal = entities.get(0);
         final ReadOnlyKeyVaultCertificateEntity firstRenewal = entities.get(1);
         final ReadOnlyKeyVaultCertificateEntity secondRenewal = entities.get(2);
-        assertTimestampsAreAdjustedAsExpected(approxNow, recreatedOriginal, INT_800);
+        assertTimestampsAreAdjustedAsExpected(approxNow, recreatedOriginal, INT_MORE_THAN_TWO_YEARS_IN_DAYS);
         final OffsetDateTime firstRenewalDay = recreatedOriginal.getExpiry().map(v -> v.minusDays(triggerThresholdDays)).orElseThrow();
         assertTimestampsAreAdjustedAsExpected(approxNow, firstRenewal, DAYS.between(firstRenewalDay, approxNow));
         final OffsetDateTime secondRenewalDay = firstRenewal.getExpiry().map(v -> v.minusDays(triggerThresholdDays)).orElseThrow();
@@ -189,7 +192,7 @@ class VaultFakeImplIntegrationTest {
         certificateVaultFake.delete(originalCertId);
 
         //when
-        underTest.timeShift(SECONDS_IN_800_DAYS, true);
+        underTest.timeShift(SECONDS_IN_MORE_THAN_TWO_YEARS, true);
 
         //then
         final boolean exists = underTest.certificateVaultFake().getDeletedEntities().containsName(originalCertId.id());
@@ -243,5 +246,25 @@ class VaultFakeImplIntegrationTest {
         Assertions.assertEquals(expectedCreationDaysAgo, DAYS.between(recreatedOriginal.getNotBefore().orElseThrow(), approxNow));
         Assertions.assertEquals(DEFAULT_VALIDITY_MONTHS, MONTHS
                 .between(recreatedOriginal.getNotBefore().orElseThrow(), recreatedOriginal.getExpiry().orElseThrow()));
+    }
+
+    private static int getMoreThanTwoYearsAvoidingLeapYear() {
+        final Instant moreThanTwoYearsAgo = Instant.now().minus(INITIAL_OFFSET_IN_DAYS, DAYS);
+        if (isEndOfFebruary(moreThanTwoYearsAgo) || isBeginningOfMarch(moreThanTwoYearsAgo)) {
+            return INITIAL_OFFSET_IN_DAYS + EXTRA_OFFSET_IN_DAYS;
+        }
+        return INITIAL_OFFSET_IN_DAYS;
+    }
+
+    @SuppressWarnings("checkstyle:MagicNumber")
+    private static boolean isBeginningOfMarch(final Instant instant) {
+        return instant.atOffset(ZoneOffset.UTC).getMonth() == Month.MARCH
+                && instant.atOffset(ZoneOffset.UTC).getDayOfMonth() < 5;
+    }
+
+    @SuppressWarnings("checkstyle:MagicNumber")
+    private static boolean isEndOfFebruary(final Instant instant) {
+        return instant.atOffset(ZoneOffset.UTC).getMonth() == Month.FEBRUARY
+                && instant.atOffset(ZoneOffset.UTC).getDayOfMonth() > 27;
     }
 }
